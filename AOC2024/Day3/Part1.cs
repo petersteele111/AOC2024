2c4e732b56e1f26f7b4c77d13e65a94fb07a8dc6
Peter Steele <petersteele111@gmail.com>
2024-12-05 15:16:59 -0500

    Day 3 part 1 Completed

---
+++ AOC2024/Day3/Part1.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AOC2024.Day3;
+
+public class Part1
+{
+    public static void Run()
+    {
+        var input = File.ReadAllText("Day3/input.txt");
+        var sum = 0;
+
+        Console.WriteLine("Starting to process input...");
+
+        // Regular expression to match valid mul(X,Y) instructions
+        var regex = new Regex(@"mul\((\d+),(\d+)\)");
+        var matches = regex.Matches(input);
+
+        foreach (Match match in matches)
+        {
+            var x = int.Parse(match.Groups[1].Value);
+            var y = int.Parse(match.Groups[2].Value);
+            var result = x * y;
+            sum += result;
+
+            Console.WriteLine($"Found valid instruction: mul({x},{y}) = {result}");
+        }
+
+        Console.WriteLine($"Sum of all valid multiplications: {sum}");
+    }
+}
---
+++ AOC2024/Day3/Part2.cs
@@ -0,0 +1,6 @@
+﻿namespace AOC2024.Day3;
+
+public class Part2
+{
+    
+}
