2d1c126d5abbf8f856fcff1af3b72250147c9ac8
Peter Steele <petersteele111@gmail.com>
2024-12-05 15:46:50 -0500

    Completed Day 3

--- AOC2024/Day3/Part2.cs
+++ AOC2024/Day3/Part2.cs
@@ -1,6 +1,60 @@
-﻿namespace AOC2024.Day3;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
-public class Part2
+namespace AOC2024.Day3
 {
-    
+    public class Part2
+    {
+        public static void Run()
+        {
+            var input = File.ReadAllText("Day3/input.txt");
+            
+            var mulRegex = new Regex(@"mul\((\d+),(\d+)\)");
+            var doRegex = new Regex(@"do\(\)");
+            var dontRegex = new Regex(@"don't\(\)");
+
+            var instructions = new SortedList<int, (string type, int x, int y)>();
+            
+            foreach (Match match in mulRegex.Matches(input))
+            {
+                instructions.Add(match.Index, ("mul", int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)));
+            }
+            
+            foreach (Match match in doRegex.Matches(input))
+            {
+                instructions.Add(match.Index, ("do", 0, 0));
+            }
+            
+            foreach (Match match in dontRegex.Matches(input))
+            {
+                instructions.Add(match.Index, ("don't", 0, 0));
+            }
+
+            bool mulEnabled = true;
+            long sum = 0;
+
+            foreach (var kvp in instructions)
+            {
+                var instruction = kvp.Value;
+                switch (instruction.type)
+                {
+                    case "do":
+                        mulEnabled = true;
+                        break;
+                    case "don't":
+                        mulEnabled = false;
+                        break;
+                    case "mul":
+                        if (mulEnabled)
+                        {
+                            sum += (long)instruction.x * instruction.y;
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Sum of enabled multiplications: {sum}");
+        }
+    }
 }
